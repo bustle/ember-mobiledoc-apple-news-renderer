3c8cdceefdf8d7505216e5e6ad4dddc599ea94be
Cory <cory.forsyth@gmail.com>
2017-04-19 15:46:42 -0400

    import mobiledoc-apple-news-renderer

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 /* eslint-env node */
 'use strict';
 
+var MergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+var path = require('path');
+
+function distDirFor(packageName) {
+  try {
+    var resolved = require.resolve(packageName + '/package.json');
+    return path.join(path.dirname(resolved), 'dist');
+  } catch (e) {
+    return null;
+  }
+}
+
 module.exports = {
-  name: 'ember-mobiledoc-apple-news-renderer'
+  name: 'ember-mobiledoc-apple-news-renderer',
+
+  treeForVendor: function() {
+    var files = []
+    var dir = distDirFor('@bustle/mobiledoc-apple-news-renderer');
+
+    files.push(new Funnel(dir, {
+      files: [
+        'amd/mobiledoc-apple-news-renderer.js',
+        'amd/mobiledoc-apple-news-renderer.map'
+      ],
+      destDir: 'mobiledoc-apple-news-renderer'
+    }))
+
+    return new MergeTrees(files)
+  },
+
+  included: function(app) {
+    app.import('vendor/mobiledoc-apple-news-renderer/amd/mobiledoc-apple-news-renderer.js')
+  }
 };
